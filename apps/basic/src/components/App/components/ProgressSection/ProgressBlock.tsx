99610496379abef6a8db4c564989c66b18ab03e0
ot07 <ot07neoki@gmail.com>
2024-01-08 02:00:52 +0900

    refactor

--- apps/basic/src/components/App/components/ProgressSection/ProgressBlock.tsx
+++ apps/basic/src/components/App/components/ProgressSection/ProgressBlock.tsx
@@ -1,4 +1,5 @@
 import { forwardRef } from 'react'
+import { Label } from '~/components/ui/label'
 
 interface ProgressBlockProps {
   label: string
@@ -7,7 +8,7 @@ interface ProgressBlockProps {
 export const ProgressBlock = forwardRef<HTMLSpanElement, ProgressBlockProps>(
   ({ label }, ref) => (
     <div className="flex items-center justify-between py-0.5">
-      <span className="text-sm font-medium leading-none">{label}</span>
+      <Label>{label}</Label>
       <span
         ref={ref}
         className="w-12 text-right text-sm text-muted-foreground"
